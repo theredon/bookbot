77064c1b165a7c6ae9f4c75d965f50b9db5b823f
Theredon <theredon@gmail.com>
2025-01-10 14:26:29 -0700

    Created main function to read file and print to console

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+def main(file):
+    with open(file) as f:
+        file_contents = f.read()
+    print(file_contents)
+    f.close()
+
+
+if __name__ == "__main__":
+    main("books/frankenstein.txt")
