2af37498fa3fb7e923fb4fc22292c9101076546f
Theredon <theredon@gmail.com>
2025-01-10 19:09:05 -0700

    added sort function for characters and a report printout

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    count = count_words(book_text)
-    print(count)
+    book_path: str = "books/frankenstein.txt"
+    book_text: str = get_book_text(book_path)
+    count: int = count_words(book_text)
+    char_count: dict = count_characters(book_text)
+    sorted_char_count = dict_to_be_sorted(char_count)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{count} words found in the document")
+    for char in sorted_char_count:
+        if not char["char"].isalpha():
+            continue                
+        print(f"The {char['char']} character was found {char['num']} times")
+    print("--- End report ---")
 
 
 def get_book_text(file: str):
@@ -14,6 +23,29 @@ def count_words(text: str):
     words = text.split()
     return len(words)
 
+
+def count_characters(text: str):
+    characters = {}
+    for c in text:
+        lowercase = c.lower()
+        if lowercase in characters:
+            characters[lowercase] += 1
+        else:
+            characters[lowercase] = 1
+    return characters
+
+
+def sorted_on(dictionary: dict):
+    return dictionary["num"]
+
+
+def dict_to_be_sorted(characters: dict):
+    sorted_list = []
+    for key in characters:
+        sorted_list.append({"char": key, "num": characters[key]})
+    sorted_list.sort(reverse=True, key=sorted_on)
+    return sorted_list
+    
 
 if __name__ == "__main__":
     main()
